8e1c2d66f9a1e064543feeb011c802d51ab2903b
EDUARDO MARTINS JÚNIOR <eduardo_martins-j@estudante.sesisenai.org.br>
2024-02-07 14:50:42 -0300

    PRIMEIRA VERSAO

---
+++ conversor.js
@@ -0,0 +1,10 @@
+
+let resistividade = 0.0172
+
+let comprimento = 0
+let areaTranversal = 1
+let resistencia = 0
+
+resistencia = (resistividade * comprimento) / areaTranversal
+
+console.log("valor da resistencia é = ", resistencia)
